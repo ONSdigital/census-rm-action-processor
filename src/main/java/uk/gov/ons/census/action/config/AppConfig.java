0db9ee10a4daeb485fd6cc8e671046ac750ca81d
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-08-06 16:50:51 +0100

    Fix Spring retries bug with transaction manager

--- src/main/java/uk/gov/ons/census/action/config/AppConfig.java
+++ src/main/java/uk/gov/ons/census/action/config/AppConfig.java
@@ -4,14 +4,19 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.TimeZone;
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.transaction.RabbitTransactionManager;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.transaction.ChainedTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -42,5 +47,20 @@ public class AppConfig {
   @Bean
   public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
     return new RabbitAdmin(connectionFactory);
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(
+      EntityManagerFactory emf, ConnectionFactory connectionFactory) {
+    JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(emf);
+    RabbitTransactionManager rabbitTransactionManager =
+        new RabbitTransactionManager(connectionFactory);
+
+    // We are using a technique described by Dr David Syer in order to synchronise the commits
+    // and rollbacks across both Rabbit and Postgres (i.e. AMQP and JPA/Hibernate/JDBC).
+    // We could have used Atomikos, but it was decided to be overkill by architects & tech leads.
+    // The original article is: Distributed transactions in Spring, with and without XA
+    // infoworld.com/article/2077963/distributed-transactions-in-spring--with-and-without-xa.html
+    return new ChainedTransactionManager(rabbitTransactionManager, jpaTransactionManager);
   }
 }
